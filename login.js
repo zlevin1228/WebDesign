eacebdb0993bb1e4a8528ec18abd3a5f2c223af4
ayatsakalla <525asakalla@frhsd.com>
2025-02-18 21:05:20 -0500

    completed log in page

---
+++ login.js
@@ -0,0 +1,18 @@
+//function used to log into the members.html page
+
+function checkPassword() {
+    const correctPassword = "bestboardever123$"; // Change this to your desired password
+    const inputPassword = document.getElementById("password").value; //grabs the password inputted
+    const message = document.getElementById("message"); //message displayed incorrect/correct pass
+
+    if (inputPassword === correctPassword) { //if password inputted is equal to correctPass
+        message.style.color = "green"; //turns the color of the message green
+        message.textContent = "Login successful!"; // message displayed if correct
+        setTimeout(() => {
+            window.location.href = "members.html"; // Redirect to another page
+        }, 1000); //time out function of 1000ms to redirect to members.html
+    } else {
+        message.style.color = "red"; //if incorrect, message turns red
+        message.textContent = "Incorrect password. Try again."; //message displayed if incorrect
+    }
+}
